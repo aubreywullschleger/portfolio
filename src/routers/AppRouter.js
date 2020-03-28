6a2462d166e767aed2706eb3e8a163a29819109f
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-03-27 19:32:59 -0500

    set up basic app router and component pages

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <h1>portfolio</h1>
+    </div>
+  </BrowserRouter>
+)
+
+export default AppRouter
